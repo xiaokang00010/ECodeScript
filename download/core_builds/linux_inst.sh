1edd5cbe7878e66a2b3c662377c8859968908e51
xiaokang00010 <zhouyongkang4321@gmail.com>
2020-08-25 08:50:40 +0800

    Update Install Script

--- download/core_builds/linux_inst.sh
+++ download/core_builds/linux_inst.sh
@@ -1,6 +1,6 @@
 echo Starting Download...
 echo If you are installed curl
-#curl https://www.00010.ml/ECodeScript/download/core_builds/ECode.Script_Release_1.zip >> /var/tmp/autoinst.zip
+curl https://www.00010.ml/ECodeScript/download/core_builds/ECode.Script_Release_1.zip >> /var/tmp/autoinst.zip
 echo Downloaded 1 files
 read -p "Install ECode.Script To(Please Input Absolute path!): " ECode_Script_Install_Path
 sudo unzip /var/tmp/autoinst.zip -d $ECode_Script_Install_Path/
