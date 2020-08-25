948f231f04f4e8dd514046afad3db56c858407eb
xiaokang00010 <zhouyongkang4321@gmail.com>
2020-08-25 08:49:16 +0800

    Update Install Script

--- download/core_builds/linux_inst.sh
+++ download/core_builds/linux_inst.sh
@@ -1,9 +1,14 @@
 echo Starting Download...
 echo If you are installed curl
-curl https://www.00010.ml/ECodeScript/download/core_builds/ECode.Script_Release_1.zip >> /var/tmp/autoinst.zip
+#curl https://www.00010.ml/ECodeScript/download/core_builds/ECode.Script_Release_1.zip >> /var/tmp/autoinst.zip
 echo Downloaded 1 files
 read -p "Install ECode.Script To: " ECode_Script_Install_Path
-sudo unzip /var/tmp/autoinst.zip -d $ECode_Script_Install_Path
+sudo unzip /var/tmp/autoinst.zip -d $ECode_Script_Install_Path/
+echo Compiling Source Code...
+sudo rm -rf $ECode_Script_Install_Path/ECode.Script/ECode.S
+sudo rm -rf $ECode_Script_Install_Path/ECode.Script/include/libecode.so
+sudo g++ $ECode_Script_Install_Path/ECode.Script/ECode.S.cpp -o $ECode_Script_Install_Path/ECode.Script/ECode.S -ldl -std=c++17
+sudo g++ $ECode_Script_Install_Path/ECode.Script/ecodedymlib.cpp -shared -fPIC -o $ECode_Script_Install_Path/ECode.Script/include/libecode.so -std=c++17
 read -p "Would You Want Add ECode.Script To PATH Please Input Absolute path!(y/n)?" ADDTOPATH
 if["$ADDTOPATH -a y"];THEN
     echo Adding...
