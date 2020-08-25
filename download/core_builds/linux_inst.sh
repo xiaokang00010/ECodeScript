72d30236411bfbe6779d41a88fd780aaacfd23be
xiaokang00010 <zhouyongkang4321@gmail.com>
2020-08-25 08:50:26 +0800

    Update Install Script

--- download/core_builds/linux_inst.sh
+++ download/core_builds/linux_inst.sh
@@ -2,14 +2,14 @@ echo Starting Download...
 echo If you are installed curl
 #curl https://www.00010.ml/ECodeScript/download/core_builds/ECode.Script_Release_1.zip >> /var/tmp/autoinst.zip
 echo Downloaded 1 files
-read -p "Install ECode.Script To: " ECode_Script_Install_Path
+read -p "Install ECode.Script To(Please Input Absolute path!): " ECode_Script_Install_Path
 sudo unzip /var/tmp/autoinst.zip -d $ECode_Script_Install_Path/
 echo Compiling Source Code...
 sudo rm -rf $ECode_Script_Install_Path/ECode.Script/ECode.S
 sudo rm -rf $ECode_Script_Install_Path/ECode.Script/include/libecode.so
 sudo g++ $ECode_Script_Install_Path/ECode.Script/ECode.S.cpp -o $ECode_Script_Install_Path/ECode.Script/ECode.S -ldl -std=c++17
 sudo g++ $ECode_Script_Install_Path/ECode.Script/ecodedymlib.cpp -shared -fPIC -o $ECode_Script_Install_Path/ECode.Script/include/libecode.so -std=c++17
-read -p "Would You Want Add ECode.Script To PATH Please Input Absolute path!(y/n)?" ADDTOPATH
+read -p "Would You Want Add ECode.Script To PATH(y/n)?" ADDTOPATH
 if["$ADDTOPATH -a y"];THEN
     echo Adding...
     echo $ECode_Script_Install_Path/ECode.Script/ECode.S
